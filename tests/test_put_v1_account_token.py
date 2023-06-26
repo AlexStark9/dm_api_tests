3006ec43c2d428f93d747d1356f5d077af1d6314
AlexStark <alex.stark3038@gmail.com>
2023-06-26 23:51:55 +0300

    Забыл добавить захардкоженный токен

--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -4,6 +4,6 @@ from services.dm_api_account import DmApiAccount
 def test_put_v1_account_token():
     api = DmApiAccount(host="http://localhost:5051")
 
-    response = api.account.put_v1_account_token()
+    response = api.account.put_v1_account_token(token="9370425b-5365-4dde-aeed-4339435e4360")
 
     print(response)
